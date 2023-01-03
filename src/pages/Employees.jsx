1c83cf129804efe81aad0c637281ff6e18be1ee7
shuhei_fujimura <shushu2525.1694@gmail.com>
2023-01-03 10:38:29 +0900

    Employeesページ作成

--- src/pages/Employees.jsx
+++ src/pages/Employees.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React from 'react';
+import {
+  GridComponent,
+  Inject,
+  ColumnsDirective,
+  ColumnDirective,
+  Search,
+  Page,
+} from '@syncfusion/ej2-react-grids';
+
+import { employeesData, employeesGrid } from '../data/dummy';
+import { Header } from '../components';
 
 const Employees = () => {
-  return (
-    <div>Employees</div>
-  )
-}
+  const toolbarOptions = ['Search'];
+  const editing = { allowDeleting: true, allowEditing: true };
 
-export default Employees
+  return (
+    <div className="m-2 md:m-10">
+      <Header category="Page" title="Employees" />
+      <GridComponent
+        dataSource={employeesData}
+        width="auto"
+        allowPaging
+        allowSorting
+        pageSettings={{ pageCount: 5 }}
+        editSettings={editing}
+        toolbar={toolbarOptions}>
+        <ColumnsDirective>
+          {employeesGrid.map((item, index) => (
+            <ColumnDirective key={index} {...item} />
+          ))}
+        </ColumnsDirective>
+        <Inject services={[Search, Page]} />
+      </GridComponent>
+    </div>
+  );
+};
+
+export default Employees;
